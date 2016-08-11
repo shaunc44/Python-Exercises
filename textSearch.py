7094882b9c4a11cdc16085c012c39ebb54c84f00
Shaun <seyyes2000@hotmail.com>
2016-08-11 06:09:49 -0400

    work on textSearch program

--- textSearch.py
+++ textSearch.py
@@ -1,2 +1,14 @@
-# Program to search for words in user-entered text
+# Program that counts words in user-entered text
 # Mininum length is 3 characters
+
+import re
+
+def count_words (text, words):
+	return 0
+
+print ('Hello, please enter a block of text to be analyzed: \n')
+text = input()
+str(text)
+print ('Thank you!\n')
+print ('Now, please enter the words you would like to count: \n')
+words = input()
