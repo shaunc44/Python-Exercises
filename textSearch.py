ac0b0a37c58d3a1f04d3b7970bc049a2a5f465a0
Shaun <seyyes2000@hotmail.com>
2016-08-14 15:01:26 -0400

    fix: counter now says 0 - work coninues

--- textSearch.py
+++ textSearch.py
@@ -1,17 +1,24 @@
-# Program that counts words in user-entered text
+# Program that counts a word from a user-entered text
 # Mininum length is 3 characters ****(maybe)
 
 def textSearch (text, words):
-	count = 0
+
 	text = text.lower()
+	words = words.lower()
+
+	text_joined = ''.join(c 
+		if c.isalnum() 
+		else ' ' for c in text).split()
+
+	count = 0
 	for i in words:
-		if i in text:
+		if i in text_joined:
 			count += 1
 
 	return str(count)
 	#return 0
 
-text = raw_input('Hi, please enter some text to be analyzed:\n\n\t')
-words = raw_input('\nWhich words would you like to count?\n\n\t')
+text = raw_input('Please enter some text to be analyzed:\n\n\t')
+words = raw_input('\nWhich word would you like to count?\n\n\t')
 
 print ('\nWord Count = ' + (textSearch (text, words)))
