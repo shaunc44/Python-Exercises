40bbc5faec31e8859b68072574d662a14263b053
Shaun <seyyes2000@hotmail.com>
2016-08-15 13:04:46 -0400

    first commit - secret message

---
+++ secretMessage.py
@@ -0,0 +1,17 @@
+# Program that separates uppercase letter thereby
+# revealing a secret message
+
+def secretMessage (msg):
+	msg_decoded = ''.join(letter
+		for letter in msg_decoded
+			if letter)
+
+
+
+
+
+# User entered message
+msg = raw_input('Enter your message to decoded: \n\n\t')
+
+# Output the decoded message
+print(msg_decoded)
