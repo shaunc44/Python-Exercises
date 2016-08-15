6a7eb00c2b1860622d99e754b9abce49ad91e623
Shaun <seyyes2000@hotmail.com>
2016-08-15 17:18:32 -0400

    first commit - three words program

---
+++ threeWords.py
@@ -0,0 +1,11 @@
+# This program check if three words exist in the 
+# user-entered text
+
+def threeWords ():
+
+
+
+
+# This is the sentence we are going to check for 
+# three consecutive words without numbers
+sentence = raw_input('Enter sentence: \n\n\t')
