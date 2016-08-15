78eecdf2c8ea6f5b1a8145ff10af09f8cd7beebf
Shaun <seyyes2000@hotmail.com>
2016-08-15 13:13:22 -0400

    exercise complete

--- secretMessage.py
+++ secretMessage.py
@@ -1,17 +1,18 @@
-# Program that separates uppercase letter thereby
-# revealing a secret message
+# Program that separates uppercase letters which will
+# reveal a secret message
 
+
+# Function to join all uppercase letters and then return them
 def secretMessage (msg):
 	msg_decoded = ''.join(letter
-		for letter in msg_decoded
-			if letter)
-
-
-
+		for letter in msg
+			if letter.isupper())
+	return str(msg_decoded)
 
 
 # User entered message
 msg = raw_input('Enter your message to decoded: \n\n\t')
 
+
 # Output the decoded message
-print(msg_decoded)
+print('\n\nYour secret message is ' + secretMessage(msg))
