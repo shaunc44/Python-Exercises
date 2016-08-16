5008e6eead3298bdb26e0370d5131fe58f162be2
Shaun <seyyes2000@hotmail.com>
2016-08-16 17:10:55 -0400

    finalize code for threeWords.py

--- threeWords.py
+++ threeWords.py
@@ -1,24 +1,24 @@
-# This program check if three words exist in the 
-# user-entered text
+# This program verifies if three words, with letters
+# only, exist in the user-entered text
 
+# This function splits the string input by each space 
+# and then counts the words that contain letters only
 def threeWords (sentence):
-	sent = sentence.lower()
-	sentSplit = sent.split()
-		for i in sentSplit:
-			if sentSplit.isalpha():
-				count += 1
-		return True
-
+	sentSplit = sentence.split()
+	count = 0
+	for word in sentSplit:
+		if word.isalpha():
+			count += 1
+	if count >=3:
+		return str(True)
 	else:
+		return str(False)
 
+# Introduction of what this program does
+print('This program will print True if your sentence has three words or more and False if it does not.\n')
 
-	return False
+# Request sentence for user-input
+sentence = raw_input('Enter a sentence: ')
 
-
-
-
-# This is the sentence we are going to check for 
-# three consecutive words without numbers
-sentence = raw_input('Enter sentence: \n\n\t')
-
-
+# Print bool result of threeWords function
+print('\n' + threeWords(sentence))
