334c6a16b7b4e003b7d1cfbaeb1ff1cb0bc48d68
Shaun <seyyes2000@hotmail.com>
2016-08-17 05:24:30 -0400

    first commit

---
+++ indexWithPower.py
@@ -0,0 +1,15 @@
+# Program that finds the Nth power of an array
+# that contains N elements
+
+def indexWithPower (array, n):
+	if (n > len(array)-1):
+		return -1
+	else:
+		return array[n]**n
+
+	indexWithPower[array, n]
+
+	return None
+
+
+
