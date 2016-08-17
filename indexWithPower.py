07758d76e637c4d451fd7f3fdb45a36fcc698220
Shaun <seyyes2000@hotmail.com>
2016-08-17 17:56:45 -0400

    update indexPower program

--- indexWithPower.py
+++ indexWithPower.py
@@ -2,15 +2,24 @@
 # in an array and returns -1 if N is outside the array
 # Example - indexWithPower([1, 3, 5, 6], 2) = 25
 
-def indexWithPower (array, n):
+def indexPower (array, n):
 	if (n > len(array)-1):
 		return -1
 	else:
 		return array[n]**n
 
-	indexWithPower[array, n]
+	indexArray[array, n]
 
-	return None
+	#return None
 
+print ('This program will take an array and raise the Nth')
+print ('element to the Nth power based on index position\n')
+print ('Example: ([1, 4, 8, 7], 2) = 64\n\n')
 
-
+num_array = list()
+num = raw_input ('Enter how many elements you want: ')
+print ('Enter each number in the array: ')
+for i in range(int(num)):
+	n = raw_input('num: ')
+	num_array.append(int(n))
+print 'Array:', num_array
