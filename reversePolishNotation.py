e5c86e687f3ea9bdd9cdfc7f996df0b00574521e
Shaun <seyyes2000@hotmail.com>
2016-08-25 17:00:11 -0400

    finish rpn2

--- reversePolishNotation.py
+++ reversePolishNotation.py
@@ -1,26 +1,32 @@
+# Function to evaluated Reverse Polish Notation
 def rpn(equation):
-# Evaluate a reverse polish notation
 
-    stack = []
+	# Create list of equation parts and call it stack
+	stack = []
 
-    for val in equation.split(' '):
-        if val in ['-', '+', '*', '/']:
-            op1 = stack.pop()
-            op2 = stack.pop()
-            if val == '-':
-                result = op2 - op1
-            if val == '+':
-                result = op2 + op1
-            if val == '*':
-                result = op2 * op1
-            if val == '/':
-                result = op2 / op1
-            stack.append(result)
-        else:
-            stack.append(float(val))
+	# For loop iterates through each value of equation
+	for val in equation.split():
+		# If operator, use to calculate 2 nums in stack
+		if val in ['-', '+', '*', '/', '**']:
+			op1 = stack.pop()
+			op2 = stack.pop()
+			if val == '-':
+				result = op2 - op1
+			if val == '+':
+				result = op2 + op1
+			if val == '*':
+				result = op2 * op1
+			if val == '/':
+				result = op2 / op1
+			if val == '**':
+				result = op2 ** op1
+			stack.append(result)
+		# If num, append to stack
+		else:
+			stack.append(float(val))
 
-    return stack.pop()
+	return stack.pop()
 
-equation = "3 4 * 8 - 3 / 9 *"
+equation = "3 4 ** 8 - 27 +"
 
 print rpn(equation)
