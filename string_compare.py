2b70aa6d9eace0f4ccbb5b2699343787e1d5c1f0
Shaun <seyyes2000@hotmail.com>
2016-08-28 06:03:22 -0400

    string_compare - add comments

--- string_compare.py
+++ string_compare.py
@@ -4,14 +4,17 @@
 
 def string_compare (string1, string2):
 	stack = []
+	#Compare letters in 1st string to 2nd string
 	for letter1 in string1.lower():
 		for letter2 in string2.lower():
 			if letter1 == letter2:
 				stack.append(letter1)
+	#Remove duplicate letters
 	stack = list(set(stack))
 	print stack
 
 
+#Enter string inputs and call function
 string1 = 'Yellow'
 string2 = 'Submarine'
 string_compare(string1, string2)
