fba4d136a0edc777c702333207bb96e53d7ebf94
Shaun <seyyes2000@hotmail.com>
2016-08-28 06:01:28 -0400

    finish string_compare

--- string_compare.py
+++ string_compare.py
@@ -1,20 +1,17 @@
+#Function to compare strings
+#Converts all input to lowercase
+#Does not return duplicates
+
 def string_compare (string1, string2):
-
 	stack = []
-
-	for letter1 in string1:
-		for letter2 in string2:
+	for letter1 in string1.lower():
+		for letter2 in string2.lower():
 			if letter1 == letter2:
 				stack.append(letter1)
-				continue
-				#return stack
-				#break
-		#else:
-		#	print ('No letters match')
-		#return stack
-		print stack
+	stack = list(set(stack))
+	print stack
 
 
-string1 = 'cox'
-string2 = 'marox'
-print (string_compare(string1, string2))
+string1 = 'Yellow'
+string2 = 'Submarine'
+string_compare(string1, string2)
