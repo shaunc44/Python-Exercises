a5fef0fd93db351289e548a3ea146d49da78bba9
Shaun <seyyes2000@hotmail.com>
2016-08-28 16:36:41 -0400

    update largest_prime

--- largest_prime.py
+++ largest_prime.py
@@ -1,11 +1,22 @@
 def largest_prime (num):
 
-	for i in range(1, num + 1):
+	divisible_nums = []
+	for i in range(2, num + 1):
 		if num % i == 0:
-			print i
+			#Create list of divisible numbers
+			divisible_nums.append(i)
 
 
-largest_prime(10)
+	for x in divisible_nums:
+		for y in range(2, x):
+			if x % y == 0:
+				z = y
+				return z, 'is the largest prime factor'
+			else:
+				return x, 'is the largest prime factor'
+
+
+print (largest_prime(11))
 
 # 1 check for primes <= input
 # 2 check if input is divisible by highest prime
