ef093dce50c6430b0954b850916a290b0ff1aee6
Shaun <seyyes2000@hotmail.com>
2016-08-29 13:27:39 -0400

    finish sieve_prime

--- sieve_prime.py
+++ sieve_prime.py
@@ -3,14 +3,14 @@ def sieve_prime(number):
 	sieve_list = []
 
 	for i in range(2, number):
-		for z in range(2, number):
+		for z in range(2, i):
 			if i % z == 0:
 				break
 		else:
 			#return i
 			sieve_list.append(i)
 
-		print sieve_list
+	return sieve_list
 
 
-print(sieve_prime(25))
+print(sieve_prime(15))
