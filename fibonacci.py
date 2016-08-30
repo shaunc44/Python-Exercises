3374df9dd18a46943a5f023475aaa297af16bda6
Shaun <seyyes2000@hotmail.com>
2016-08-30 06:16:13 -0400

    update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,29 @@
 def fibonacci(num):
 
 	i = 0
+	k = 1
+
+	while k < num:
+		if i > 1:
+			result =  i + k
+			print result
+			i = k
+			k = result
+		else:
+			print i
+			i += 1
+
+
+
+	"""
 	while i < num:
-		print i
-		i += 1
+		if i > 1:
+			result = (i + (i-1))
+			return result
+			i += 1
+		else:
+			print i
+			i += 1
+	"""
 
 fibonacci(5)
