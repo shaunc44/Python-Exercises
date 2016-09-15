942f6a4d514139a68bf8a01019b936b292872e1d
Shaun <seyyes2000@hotmail.com>
2016-09-15 17:39:47 -0400

    update number_base.py

--- number_base.py
+++ number_base.py
@@ -1,14 +1,22 @@
-def number_base():
+def number_base(num, base):
 
+	num = list(num)
+	num = num.reverse()
 
+	#reverse order of nums 
+	for x in num:
+		#for x in num: #do I need this for loop
+		if num[x].isalpha():
+			num[y] = ord(num[x]) - 55
+		else:
+			num[y] = num[x]
 
+	for z in num:
+		total += num[z] * base ** z
 
+	return total
 
-
-
-
-#Input #1: number as string
-#Input #2: radix (base) as integer
-print number_base("AF", 16)
-#Should print ((10 * 16^1) + (15 * 16^0))
-#Which equals 160 + 15 = 175
+	print number_base("AF", 16)
+	#Should equal 175
+	#AF = (Abase16 * 16^1) + (Fbase16 * 6^0)
+	#(10*16)+(15*1)=160+15=175
