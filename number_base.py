192ac1df59439d60096c6db1a0d328c7c5e0332e
Shaun <seyyes2000@hotmail.com>
2016-09-15 05:49:23 -0400

    set up number_base.py

---
+++ number_base.py
@@ -0,0 +1,14 @@
+def number_base():
+
+
+
+
+
+
+
+
+#Input #1: number as string
+#Input #2: radix (base) as integer
+print number_base("AF", 16)
+#Should print ((10 * 16^1) + (15 * 16^0))
+#Which equals 160 + 15 = 175
