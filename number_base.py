bf3cb7e30eb642e91acc13bad4a55035d085968e
Shaun <seyyes2000@hotmail.com>
2016-09-17 07:06:17 -0400

    update num base radix program; need to implement base check

--- number_base.py
+++ number_base.py
@@ -1,24 +1,34 @@
-def number_base(num, base):
+def number_base(char, base):
 
 	#Divide string into individual chars
-	num = list(num)
+	char = list(char)
 	#Reverse chars
-	num = num[::-1]
+	char = char[::-1]
 
-	for x in num:
-		#for x in num: #do I need this for loop
-		if num[x].isalpha():
-			num[x] = ord(num[x]) - 55
+	#Create empty list to insert converted chars
+	num = list()
+	for x in char:
+		if x.isalpha():
+			n = ord(x) - 55
+			num.append(int(n))
 		else:
-			num[x] = num[x]
+			n = int(x)
+			num.append(int(n))
+
+	#Need to check whether num exceeds bases, return -1
+
+	#return num
 
 	total = 0
+	i = 0
 	for z in num:
-		total += num[z] * base ** z
+		total += (z * (base ** i))
+		i += 1
 
 	return total
 
+
 print number_base('AF', 16)
 	#Should equal 175
-	#AF = (Abase16 * 16^1) + (Fbase16 * 6^0)
+	#AF = (Abase16 * 16^1) + (Fbase16 * 16^0)
 	#(10*16)+(15*1)=160+15=175
