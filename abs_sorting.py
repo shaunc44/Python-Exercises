593a2526918dd7248f6ae40284d5e6117ce3d10c
Shaun <seyyes2000@hotmail.com>
2016-09-19 06:37:24 -0400

    update absolute sorting program

--- abs_sorting.py
+++ abs_sorting.py
@@ -1,18 +1,20 @@
-def abs_sorting(num_tuple):
+def abs_sorting(*num_tuple):
 
-	num_tuple = list(num_tuple)
-	num_list = []
-	n = 0
-	for num in num_tuple:
-		if abs(num) > n:
-			num_list.append(abs(num))
+	num_list = list(num_tuple)
+	#num_list = []
+	#n = 0
 
-
-
-
-
-
-
+	a, b, x = 0, 1, 0
+	while x < len(num_list):
+		#for (num + 1) in num_list:
+		if abs(num_list[a]) > abs(num_list[b]):
+			num_list[a] = num_list[b]
+			a += 1
+			b += 1
+		else:
+			a += 1
+			b += 1
+		x += 1
 
 
 print abs_sorting(-20, 5, -9, 30)
