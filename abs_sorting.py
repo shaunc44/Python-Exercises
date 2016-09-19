963821ca4cc32d6bd8fd2e4a074751c72958d1d1
Shaun <seyyes2000@hotmail.com>
2016-09-19 17:22:18 -0400

    finish absolute sorting program

--- abs_sorting.py
+++ abs_sorting.py
@@ -1,17 +1,13 @@
 def abs_sorting(*num_tuple):
 
 	num_list = list(num_tuple)
-	a = 0
 
-	for x in num_list:
-		for y in num_list:
-			if abs(x) > abs(y):
-				a = x
-				x = y
-				y = a
-	print num_list
-			#else:
-			#	continue
-			#return num_list
+	for x in range(0, len(num_list)):
+		for y in range(0, len(num_list)):
+			if abs(num_list[x]) < abs(num_list[y]):
+				a = num_list[x]
+				num_list[x] = num_list[y]
+				num_list[y] = a
+	return num_list
 
-print abs_sorting(-20, 5)
+print abs_sorting(-4, 3, 2, 1)
