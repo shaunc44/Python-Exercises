fb2fd44d77f30c41896fbee73694a3bc6833c882
Shaun <seyyes2000@hotmail.com>
2016-09-22 13:12:19 -0400

    update flatten dict program

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,7 +1,16 @@
 def flat_dict(dict):
 
+	for key in dict.keys():
+		for value in dict.values():
+			print key, value
+			#print value
+
+
+"""
 	for key, value in dict.items():
 		print "\"" + str(key) + "\"/\"" + str(value) + "\""
+"""
+
 
 
 dict = {'Name': 'Shaun',
