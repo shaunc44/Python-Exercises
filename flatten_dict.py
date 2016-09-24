c19fdf3693935de4c8cc467187e85897726e1245
Shaun <seyyes2000@hotmail.com>
2016-09-24 07:31:35 -0400

    update flatten dict program

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,21 +1,35 @@
 def flat_dict(dict):
 
+	for i in range(len(dict.keys())):
+		#for j in range(len(dict.values())):
+		#print "\"" + dict.keys()[i] + "\"/\"" + dict.values()[i] + "\""
+		#if dict.values()[i]
+		print dict.get('Kids')[1]
+
+#*** use this to print each value
+#dict.get('keyName')[0]
+
+
+"""
+	i = 0
+	for i in value:
+		for key, value[i] in dict.items():
+			print "\"" + str(key) + "\"/\"" + str(value) + "\""
+"""
+
+"""
 	for key in dict.keys():
+		print key
 		for value in dict.values():
-			print key, value
-			#print value
-
-
+			print value
 """
-	for key, value in dict.items():
-		print "\"" + str(key) + "\"/\"" + str(value) + "\""
-"""
+
 
 
 
 dict = {'Name': 'Shaun',
 		'Age': 99,
-		'Kids': ('Scrunchy', 'Pumpkin')
+		'Kids': ['Scrunchy', 'Pumpkin']
 		}
 
 flat_dict(dict)
