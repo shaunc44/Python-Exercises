219a11c299ca7b8a1e1897505f74160c6fd4a58e
Shaun <seyyes2000@hotmail.com>
2016-09-25 14:49:39 -0400

    update flatten dict program2

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,7 +1,18 @@
-def flat_dict(dict):
+def flat_dict(person):
 
+	for key in person.values():
+		for value in key:
+			print value
+
+"""
+	for key, value in person.items():
+		print key, value
+"""
+
+
+"""
 	key_list = dict.keys()
-	#print key_list[0]
+	print key_list
 	#print dict.get(1)[0]
 
 	for i in range(len(dict.keys())):
@@ -9,24 +20,21 @@ def flat_dict(dict):
 		#for j in range(len(dict.values())):
 		#print "\"" + dict.keys()[i] + "\"/\"" + dict.values()[i] + "\""
 		#if dict.values()[i]
-		print i
-		print dict.get(key_list[i], [1])
+		#print i
+		print dict.get(key_list[1])
 
 #*** use this to print each value
 #dict.get('Kids')[0]
 
-
-"""
 	i = 0
 	for i in value:
 		for key, value[i] in dict.items():
 			print "\"" + str(key) + "\"/\"" + str(value) + "\""
 """
 
-
-dict = {'Name': 'Shaun',
-		'Age': 99,
+person = {'Name': 'Shaun',
+		'Age': '97',
 		'Kids': ['Scrunchy', 'Pumpkin']
 		}
 
-flat_dict(dict)
+flat_dict(person)
