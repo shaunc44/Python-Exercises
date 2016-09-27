19275497669fed85003800ae028cbcec81a8b885
Shaun <seyyes2000@hotmail.com>
2016-09-27 17:18:46 -0400

    finish flatten dict program

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,4 +1,3 @@
-"""
 def flat_dict(person):
 
 	stack = [((), person)]
@@ -6,62 +5,29 @@ def flat_dict(person):
 	while stack:
 		path, current = stack.pop()
 		for k, v in current.items():
-			if isinstance(v, dict):
+			if isinstance(v, dict) and v:
 				stack.append((path + (k,), v))
 			else:
-				result["/".join((path + (k,)))] = v
+				result["/".join((path + (k,)))] = v or ''
 	return result
 
 
-person = {'Name': {'Shaun'},
-		'Age': {97},
-		'Kids': {'Scrunchy', 'Pumpkin'}
+person = {
+			'Name': {
+				'First': 'Shaun',
+				'Last': 'C'
+			},
+			'Age': '97',
+			'Kids': {
+				'Son': 'Scrunchy',
+				'Daughter': 'Pumpkin'
+			}
 		}
 
 print flat_dict(person)
-"""
 
-nums = [3, 4, 6]
-stack = [((), nums)]
-print stack
-print stack.pop()
-
-"""
-	for key in person.keys():
-		print key
-
-
-	#This prints each value on separate lines
-	for key in person.values():
-		for value in key:
-			print value
-"""
-
-
-"""
-	for key, value in person.items():
-		print key, value
-"""
-
-
-"""
-	key_list = dict.keys()
-	print key_list
-	#print dict.get(1)[0]
-
-	for i in range(len(dict.keys())):
-	#for i, j in dict.items():
-		#for j in range(len(dict.values())):
-		#print "\"" + dict.keys()[i] + "\"/\"" + dict.values()[i] + "\""
-		#if dict.values()[i]
-		#print i
-		print dict.get(key_list[1])
-
-#*** use this to print each value
-#dict.get('Kids')[0]
-
-	i = 0
-	for i in value:
-		for key, value[i] in dict.items():
-			print "\"" + str(key) + "\"/\"" + str(value) + "\""
-"""
+#Should print something like:
+#'Name'/'First': 'Shaun'
+#'Name'/'Last': 'C'
+#'Age'/'97'
+#........
