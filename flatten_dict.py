4f36f5a4270deb6cfe71001e43b22363d201168b
Shaun <seyyes2000@hotmail.com>
2016-09-28 06:15:42 -0400

    add comments to flatten dict program

--- flatten_dict.py
+++ flatten_dict.py
@@ -1,14 +1,34 @@
 def flat_dict(person):
 
+	#Puts tuple, dictionary in 'stack' list
 	stack = [((), person)]
+	print '1:', stack
+
+	#Creates empty dictionary
 	result = {}
+	print '2:', result
+
+	#While stack is true???
 	while stack:
+
+		#Unpacks stack's tuple in 'path' and stack's 'dictionary' in current
 		path, current = stack.pop()
+		print '3:', path
+		print '4:', current
+
+		#Iterate through key, value of current which is a dict
 		for k, v in current.items():
+			print '5:', k, v
+
+			#Checks that v is type dictionary and v exists
 			if isinstance(v, dict) and v:
+				print '6:', v
 				stack.append((path + (k,), v))
+				print '7:', stack
 			else:
+				#What the heck does this do????
 				result["/".join((path + (k,)))] = v or ''
+				print '8:', result
 	return result
 
 
@@ -24,10 +44,7 @@ person = {
 			}
 		}
 
-print flat_dict(person)
+flat_dict(person)
 
 #Should print something like:
-#'Name'/'First': 'Shaun'
-#'Name'/'Last': 'C'
-#'Age'/'97'
-#........
+#{'Age'/'97', 'Name'/'First': 'Shaun' .....
