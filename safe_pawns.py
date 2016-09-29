87b89ea01d3fa4b5fa9777544e5d90caba067947
Shaun <seyyes2000@hotmail.com>
2016-09-29 13:05:37 -0400

    work on safe_pawn.py

--- safe_pawns.py
+++ safe_pawns.py
@@ -1,12 +1,33 @@
-def safe_pawns(*loc):
+def safe_pawns(pawns):
 
+	print pawns
 	count = 0
-	for x in loc:
+	pawns = pawns.items()
+	print pawns
+
+	for pawn in pawns:
+		print pawn(0)
+		location = list(pawn)
+		print list(location)
+		letter = location[0]
+		num = location[1]
+		letter_dec = ord(letter)
+		pawn1 = chr(letter_dec + 1)
+		pawn2 = chr(letter_dec - 1)
+		pawn1 = letter1 + str(num - 1)
+		pawn2 = letter2 + str(num + 1)
+		new_num = 2 - 1
+
+
+"""
 		if x and ( or ):
 
+		if c2
 
 
-
+	pawn1 = .split(c2), c - one letter, 2 - 1, then rejoin
+	pawn2 =
+"""
 
 
 # Enter up to 8 pawns (coordinates) from a1 to h8
