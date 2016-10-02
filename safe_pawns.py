93a5eac123118aae58cb921340e13aa6943cb052
Shaun <seyyes2000@hotmail.com>
2016-10-02 15:51:08 -0400

    finish safe pawn program

--- safe_pawns.py
+++ safe_pawns.py
@@ -3,31 +3,24 @@ def safe_pawns(pawns):
 	count = 0
 
 	for pawn in pawns:
-		#place target pawn coordinates in vars: col & num
+		#place target pawn y, x coordinates in vars: col & num
 		#convert col(letter) to ascii value
 		col_pawn0 = ord(pawn[0])
+		#convert num coord to int
 		num_pawn0 = int(pawn[1])
 		#Determine coords of required support pawns
-		col_pawn1 = chr(col_pawn0 - 1)
-		col_pawn2 = chr(col_pawn0 + 1)
-		num_pawn1 = num_pawn2 = str(num_pawn0 - 1)
-		pawn1 = col_pawn1 + num_pawn1
-		pawn2 = col_pawn2 + num_pawn2
-		print pawn, pawn1, pawn2
+		pawn1 = chr(col_pawn0 - 1) + str(num_pawn0 - 1)
+		pawn2 = chr(col_pawn0 + 1) + str(num_pawn0 - 1)
 
-		#Create better logic statement
-		#If pawn1 or pawn 2 matches any pawns from input ...
+		#Determine if pawn is safe by comparing to need support pawns
 		for i in pawns:
-			if (pawn1 == i):
+			if (i == pawn1 or i == pawn2):
 				count += 1
-				print count
 				break
-			elif (pawn2 == i):
-				count += 1
-				print count
+
 	print count
 
 
 # Enter up to 8 pawns (coordinates) from a1 to h8
-safe_pawns({'b4', 'd4', 'f4', 'c3', 'e3', 'g5', 'd2'})
+safe_pawns({'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'e5'})
 # == 6
