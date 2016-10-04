a22cb62252c985bf075b16f96236cb6a351d28e7
Shaun <seyyes2000@hotmail.com>
2016-10-04 05:34:48 -0400

    work on roman numeral converter

--- roman_converter.py
+++ roman_converter.py
@@ -1,12 +1,16 @@
+#Create list of pairs with zip method
+#Create tuple of the list
+numerals_map = tuple(zip(
+	(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+	('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+))
+
+
 #Convert integers to roman numerals
 def roman_converter (num):
 
-	#Create list of pairs with zip method
-	#Create tuple of the list
-	numerals_map = tuple(zip(
-		(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
-		('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-	))
+	result []
+
 
 	if num in numerals_map:
 		print numerals_map(num[1])
