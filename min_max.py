73d00a63c0bc73b4e28edfe225ea57d23e05184a
Shaun <seyyes2000@hotmail.com>
2016-10-05 05:41:55 -0400

    set up min_max.py

---
+++ min_max.py
@@ -0,0 +1,11 @@
+def minimum(*args):
+	return min(*args)
+
+def maximum(*args):
+	return max(*args)
+
+
+nums = (3, 2, 8, 51, 9, 4, 1, 0, -3)
+
+print 'Max:', maximum(nums)
+print 'Min:', minimum(nums)
