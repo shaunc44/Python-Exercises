5a3366ab3164061eefe5c9814163a04050b9fb40
Shaun <seyyes2000@hotmail.com>
2016-10-05 17:18:48 -0400

    update min_max.py

--- min_max.py
+++ min_max.py
@@ -10,14 +10,19 @@ def min(*args):
 
 #Create function to find max number
 def max(*args):
-	args = list(args)
-	print args
-	high = args[0]
-	for i in args:
+	#args = list(args)
+	#print type(args)
+	#args.split()
+	#print args[0][3]
+	high = args[0][0]
+	#print high
+	for i in args[0]:
 		if i > high:
 			high = i
 	return high
 
+#print list('hello', 'shaun')
+
 
 print 'Min:', min(-3, 2, 51, 9, 4, 51, 0, -3)
-print 'Max:', max([1, 2, 0, 3, 4])
+print 'Max:', max([1, 2, 0, 33, 4], 7)
