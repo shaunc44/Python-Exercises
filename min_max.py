899e0fbaf97dd3b19672c4805991b227ce4e65d7
Shaun <seyyes2000@hotmail.com>
2016-10-05 13:06:02 -0400

    update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,18 +1,23 @@
+#Create function to find min number
 def min(*args):
-
-	#nums_list = map(list, args)
-	print args
+	args = list(args)
+	low = args[0]
 	for i in args:
-		print i
-
-	#return min(*args)
-
-
-#def max(*args):
-	#return max(*args)
+		if i < low:
+			low = i
+	return low
 
 
-#nums = (3, 2, 8, 51, 9, 4, 1, 0, -3)
+#Create function to find max number
+def max(*args):
+	args = list(args)
+	print args
+	high = args[0]
+	for i in args:
+		if i > high:
+			high = i
+	return high
 
-print 'Min:', min(3, 2, 8, 51, 9, 4, 1, 0, -3)
-#print 'Max:', max(nums)
+
+print 'Min:', min(-3, 2, 51, 9, 4, 51, 0, -3)
+print 'Max:', max([1, 2, 0, 3, 4])
