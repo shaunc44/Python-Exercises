590f34e74e9cb69dfe4862de9398e182d9d59359
Shaun <seyyes2000@hotmail.com>
2016-12-02 13:30:37 -0500

    work on greeter terminal app

--- greeter.py
+++ greeter.py
@@ -1,21 +1,28 @@
 from time import sleep
+import os
 
 # Greeter is a terminal application that greets old friends warmly,
 # and remembers new friends.
 
-# Display a title bar.
-print("\t**********************************************")
-print("\t***  Greeter - Hello old and new friends!  ***")
-print("\t**********************************************")
+def display_title_bar():
+	# Clear the screen before listing names
+	os.system('clear')
+
+	print("\t**********************************************")
+	print("\t***  Greeter - Hello old and new friends!  ***")
+	print("\t**********************************************")
+
 
 # Print a bunch of information, in short intervals
 names = ['aaron', 'brenda', 'cyrene', 'david', 'eric']
 
 # Print each name 5 times
 for name in names:
-	# Pause for 1 second bw batches and then skip two lines
-	sleep(1)
+	display_title_bar()
 	print("\n\n")
 
 	for x in range(0,5):
 		print (name.title())
+
+	# Pause for 1 second bw batches
+	sleep(1)
