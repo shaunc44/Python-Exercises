4a1813dcc3fdb319608c24325fc1e98b30abe706
Shaun <seyyes2000@hotmail.com>
2016-12-03 23:39:49 -0500

    work on greeter.py

--- greeter.py
+++ greeter.py
@@ -1,4 +1,3 @@
-from time import sleep
 import os
 
 # Greeter is a terminal application that greets old friends warmly,
@@ -15,27 +14,40 @@ def display_title_bar():
 	print("\t***  Greeter - Hello old and new friends!  ***")
 	print("\t**********************************************")
 
-
-#### MAIN PROGRAM ####
-
-# Set up a loop where users can choose what they'd like to do.
-choice = ''
-while choice != 'q':
-	display_title_bar()
-
+def get_user_choice():
 	# Let users know what they can do.
 	print("\n[1] See a list of friends.")
 	print("[2] Tell me about someone new.")
 	print("[q] Quit.\n")
 
-	choice = input("What would you like to do? ")
+	return input("What would you like to do? ")
+
+
+#### MAIN PROGRAM ####
+
+# Set up a loop where users can choose what they'd like to do.
+name = []
+
+choice = ''
+display_title_bar()
+while choice != 'q':
+
+	choice = get_user_choice()
 
 	# Respond to the user's choice.
+	display_title_bar()
 	if choice == '1':
 		print("\nHere are the people I know.\n")
+		for name in names:
+			print(name.title())
 	elif choice == '2':
-		print("\nI can't wait to meet this person!\n")
+		new_name = input("\nPlease tell me this preson's name: ")
+		names.append(new_name)
+		print("\nI'm so happy to know %s!\n" % new_name.title())
 	elif choice == 'q':
 		print("\nThanks for playing. Bye.")
 	else:
 		print("\nI didn't understand that choice.\n")
+
+
+
