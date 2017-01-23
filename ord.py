024987ca01dde3fe582da3f87cc13f90beae593a
Shaun <seyyes2000@hotmail.com>
2017-01-23 09:52:17 -0500

    create ord() presentation

---
+++ ord.py
@@ -0,0 +1,12 @@
+#ord() helps you find the ASCII value of a character
+#American Standard Code for Information Interchange
+
+letter = ord("A")
+
+print (letter)
+
+
+# string = "It is 40 degrees today"
+
+# for i in string:
+# 	print ( ord(i) )
